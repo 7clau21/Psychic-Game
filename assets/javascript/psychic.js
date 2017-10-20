326619f67d12cf23b5937dc1956be9d89239121f
Claudia <claudia@dhcp-v091-164.mobile.uci.edu>
2017-10-19 20:25:53 -0700

    updated links

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -1,3 +1,7 @@
+var userName = prompt("Hi! What is your name?");
+alert ("Welcome " + userName + " , type any letter to beging playing!")
+
+
 //Letter choices available
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -50,7 +54,7 @@ var reset = function() {
 document.onkeyup = function(event) {
     guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-//pushes user guess to guessed kletters
+//pushes user guess to guessed letters
   guessedLetters.push(userGuess);
   updateGuessesLeft();
   updateGuessesSoFar();
